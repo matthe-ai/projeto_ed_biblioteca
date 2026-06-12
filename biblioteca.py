f43290ed1baf0c7113294c21faf7ce76ea0a005f
Mattheus Oliveira <matthheuskaua2020@gmail.com>
2026-06-11 22:20:33 -0300

    lista encadeada

---
+++ biblioteca.py
@@ -0,0 +1,12 @@
+"""
+Funcionalidades:
+    cadastrar livro
+    remover livro
+    buscar livro
+    listar livros em ordem alfabetica
+    realizar emprestimo
+    realizar devolução
+    inserir em lista de espera quando não houver exemplares disponiveis
+    desfazer ultimos emprestimos
+    gerar relatorio geral do acervo
+"""
---
+++ utils/livro.py
@@ -0,0 +1,7 @@
+class Livro:
+    def __init__(self, ISBN:str, titulo:str, autor:str, ano_pub:int, qtd_ex:int):
+        self.isbn = ISBN
+        self.titulo = titulo
+        self.autor = autor
+        self.ano_pub = ano_pub
+        self.qtd_ex = qtd_ex
