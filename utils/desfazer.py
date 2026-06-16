d96e3cf7095fb570204b89c1d8649830b594832f
Luiz Gustavo <luiz.gustavo.corp@gmail.com>
2026-06-16 15:29:35 -0300

    Adicionado desfazer.py e org_livros.py
    org_livros.py foi implementado como árvore AVL
    
    desfazer.py foi implementado como pilha encadeada

--- utils/desfazer.py
+++ utils/desfazer.py
@@ -1,9 +1,33 @@
 """
 Pilha para controle de historico
 """
+class Node:
+    def __init__(self, dado):
+        self.dado = dado
+        self.prox = None
 
 class Historico:
     def __init__(self):
-        pass
+        self.topo = None
 
+    def vazia(self):
+        return self.topo is None
 
+    def push(self, operacao):
+        novo = Node(operacao)
+        novo.prox = self.topo
+        self.topo = novo
+
+    def pop(self):
+        if self.vazia():
+            return None
+
+        operacao = self.topo.dado
+        self.topo = self.topo.prox
+        return operacao
+
+    def topo_pilha(self):
+        if self.vazia():
+            return None
+
+        return self.topo.dado
